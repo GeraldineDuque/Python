bdd9ff85f0699d36edb3eeab29761bc20bf68f1b
unknown <gospina@cesde.net>
2024-02-06 21:41:07 -0500

    Guardand cambios

--- Taller.git/main.py
+++ Taller.git/main.py
@@ -10,3 +10,14 @@ valor_hora_extra = float(input("Ingrese el valor por hora extra: "))
 salario_base_total = horas_trabajadas * salario_base
 salario_extra = horas_extra * valor_hora_extra
 salario_total = salario_base_total + salario_extra
+
+
+print("\nColilla de Pago")
+print("Nombre: {}".format(nombre))
+print("Horas Trabajadas: {}".format(horas_trabajadas))
+print("Salario Base por Hora: ${}".format(salario_base))
+print("Salario Base Total: ${}".format(salario_base_total))
+print("Horas Extra: {}".format(horas_extra))
+print("Valor Hora Extra: ${}".format(valor_hora_extra))
+print("Salario por Horas Extra: ${}".format(salario_extra))
+print("Salario Total: ${}".format(salario_total))
